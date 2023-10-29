01132aef07cba38b7abd372b477ab903689b95bf
Maitreya25 <maitreyapatni25@gmail.com>
2023-10-29 17:27:07 +0000

    Launcher3: Only enable icon size tweaks when Themed Icons are enabled
    Change-Id: I9b6b79fe356af63a17191b5c3cfeeaf1fa1b51f6

--- src/com/android/launcher3/dragndrop/DragView.java
+++ src/com/android/launcher3/dragndrop/DragView.java
@@ -65,6 +65,7 @@ import com.android.launcher3.icons.FastBitmapDrawable;
 import com.android.launcher3.icons.IconNormalizer;
 import com.android.launcher3.model.data.ItemInfo;
 import com.android.launcher3.util.RunnableList;
+import com.android.launcher3.util.Themes;
 import com.android.launcher3.util.ViewEx;
 import com.android.launcher3.views.ActivityContext;
 import com.android.launcher3.views.BaseDragLayer;
@@ -331,7 +332,10 @@ public class DragView extends FrameLayout {
                 }
 		Rect mRect = new Rect();
 		mRect.set(bounds);
-        	mRect.inset(bounds.width()/4, bounds.height()/4);
+
+        	if(Themes.isNosThemedIconsEnabled(getContext()))
+		    mRect.inset(bounds.width()/4, bounds.height()/4);
+
 		mFgSpringDrawable.setBounds(mRect);
 
                 new Handler(mActivity.asContext().getMainLooper()).post(() ->
