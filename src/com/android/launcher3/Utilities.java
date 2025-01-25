af596b7e767dae65b91a62c3e443127360c6d6bd
HeCheng Yu <kde-yyds@qq.com>
2025-01-25 16:14:44 +0800

    Launcher3: Add an option to enable/disable background blur at app launch/closing
    Co-authored-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -1073,4 +1073,8 @@ public final class Utilities {
     public static void startLmoFreeform(Context context, ComponentName activity) {
         startLmoFreeform(context, activity, UserHandle.myUserId());
     }
+
+    public static boolean blurBackgroundAtAppLaunch(Context context) {
+        return LauncherPrefs.BLUR_BACKGROUND_AT_APP_LAUNCH.get(context);
+    }
 }
